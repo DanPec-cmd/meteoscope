7898b17079bf01684725b5b783762e33a006bb00
Danimir <danimir.pecur@protonmail.com>
2026-07-05 12:32:25 +0200

    main.py CORS allow origin added

--- backend/app/main.py
+++ backend/app/main.py
@@ -9,7 +9,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "http://127.0.0.1:5173"
+        "http://127.0.0.1:5173",
+        "https://meteoscope.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
