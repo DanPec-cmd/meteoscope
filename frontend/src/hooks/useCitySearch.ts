d11e91915588750822c8eee851117371de5e7d52
Danimir <danimir.pecur@protonmail.com>
2026-07-05 13:18:52 +0200

    Changes for Docker

--- frontend/src/hooks/useCitySearch.ts
+++ frontend/src/hooks/useCitySearch.ts
@@ -4,10 +4,10 @@ import { api } from "../services/api";
 export function useCitySearch() {
   const [results, setResults] = useState<any[]>([]);
 
-  const search = async (name: string) => {
+  const search = async (city: string) => {
     try {
       const res = await api.get("/search", {
-        params: { city: name }
+        params: { city }
       });
 
       setResults(Array.isArray(res.data) ? res.data : []);
--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://meteoscope.onrender.com/",
+  baseURL: "http://localhost:8000"
 });
